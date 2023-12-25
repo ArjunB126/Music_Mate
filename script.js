028a57cf2e934484fcad582552b79085501cb4ac
ArjunB126 <arjunbajare86@gmail.com>
2023-12-26 00:02:41 +0530

    Updated_Js

--- script.js
+++ script.js
@@ -10,14 +10,15 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Asal Mein", filePath: "/Songs Asal Mein" , coverPath: "Covers/Asal_Mein.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "", filePath: "" , coverPath: "Cover/Asal_Mein.jpg"},
+    {songName: "", filePath: "", coverPath: "Cover/Ek_tarfa.jpg"},
+    {songName: "", filePath: "", coverPath: "covers/3.jpg"},
+    {songName: "", filePath: "", coverPath: "covers/4.jpg"},
+    {songName: "", filePath: "", coverPath: "covers/5.jpg"},
+    {songName: "", filePath: "", coverPath: "covers/6.jpg"},
+    {songName: "", filePath: "", coverPath: "covers/7.jpg"},
+    {songName: "", filePath: "", coverPath: "covers/8.jpg"},
+    {songName: "", filePath: "", coverPath: "covers/9.jpg"},
+    {songName: "", filePath: "", coverPath: "covers/10.jpg"},
 ]
+
