a481c42cc8184d4bd78ca1c7eea79ef00f3a4895
ArjunB126 <arjunbajare86@gmail.com>
2023-12-26 17:12:17 +0530

    Updates

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ console.log("Welcome to Music_Mate");
 
 // Initialize the Variables
 let songIndex = 0;
-let audioElement = new Audio('Songs/ Asal Mein.mp4');
-let masterPlay = document.getElementById('masterPlay');
-let myProgressBar = document.getElementById('myProgressBar');
-let gif = document.getElementById('gif');
-let masterSongName = document.getElementById('masterSongName');
-let songItems = Array.from(document.getElementsByClassName('songItem'));
+let audioElement = new Audio('Asal Mein.mp4');
+audioElement.play();
+
+
+// let masterPlay = document.getElementById('masterPlay');
+// let myProgressBar = document.getElementById('myProgressBar');
+// let gif = document.getElementById('gif');
+// let masterSongName = document.getElementById('masterSongName');
+// let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
     {songName: "", filePath: "" , coverPath: "Cover/Asal_Mein.jpg"},
